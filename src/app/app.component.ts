e34a1b24311a37dd06986e2a9611be08bf8efc51
Fernanda velazquez <fervlazq@gmail.com>
2018-11-11 19:02:53 -0600

    api brings both users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,22 @@ import { ApiService } from './api.service';
 export class AppComponent {
   title = 'gitApp';
   usr: any = [];
+  usr2: any = [];
 
   constructor(private apiService: ApiService){}
 
-  getUsr(){
+  getUsr(form){
     this.usr = [];
-    let usrnm = 'fervlazq';
+    this.usr2 = [];
+    let usrnm = form.value.one;
+    let usrnm2 = form.value.two;
     this.apiService.getUser(usrnm).subscribe((data: {}) => {
       console.log(data);
       this.usr = data;
+    });
+    this.apiService.getUser(usrnm2).subscribe((data: {}) => {
+      console.log(data);
+      this.usr2 = data;
     });
   }
 
